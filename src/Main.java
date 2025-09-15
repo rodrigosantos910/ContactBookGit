ff142ad8b411a2eda7b518ee4086d0d9488a5af2
Rodrigo <ro.santos@campus.fct.unl.pt>
2025-09-16 00:59:14 +0100

    Main completada.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_NUMBER  = "GN";
+    public static final String EXTRA_PHONES = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +24,9 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String CONTACT_NOT_EXISTS = "Phone number does not exist.";
+    public static final String EXTRA_PHONES_EXIST = "There are contacts that share phone numbers.";
+    public static final String NO_EXTRA_PHONES = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -53,8 +58,15 @@ public class Main {
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
                     break;
+                case GET_NUMBER:
+                    getNumber(in,cBook);
+                    break;
+                case EXTRA_PHONES:
+                    getExtraPhones(cBook);
+                    break;
                 default:
                     System.out.println(COMMAND_ERROR);
+                    break;
             }
             System.out.println();
             comm = getCommand(in);
@@ -62,6 +74,25 @@ public class Main {
         System.out.println(QUIT_MSG);
         System.out.println();
         in.close();
+    }
+
+    private static void getExtraPhones(ContactBook cBook) {
+        if (cBook.hasExtraPhones()){
+            System.out.println(EXTRA_PHONES_EXIST);
+        } else {
+            System.out.println(NO_EXTRA_PHONES);
+        }
+    }
+
+    private static void getNumber(Scanner in, ContactBook cBook) {
+        int phone = in.nextInt();
+        in.nextLine();
+
+        if (cBook.getContact(phone) == null){
+            System.out.println(CONTACT_NOT_EXISTS);
+        } else {
+            System.out.println(cBook.getContact(phone).getName());
+        }
     }
 
     private static String getCommand(Scanner in) {
